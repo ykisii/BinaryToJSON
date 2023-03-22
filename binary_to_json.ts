8d875823f3d727c89b71cadbc7f4b71067cc6dd6
ykisii <ishiiyuuki@gmail.com>
2023-03-22 20:01:32 +0900

    Add readByte function

--- binary_to_json.ts
+++ binary_to_json.ts
@@ -1,20 +1,49 @@
 import { BinaryReader } from "https://deno.land/x/binary_reader@v0.1.6/mod.ts";
 
 export class BinaryToJSON {
-    // constructor(private buffer: Uint8Array) {
-    // 	if (!buffer) {
-    // 	    return;
-    // 	}
-    // 	this.buffer = buffer;
-    // }
-
     convert(buf:Uint8Array, formatFilePath: string):{} {
 	const format = JSON.parse(Deno.readTextFileSync(formatFilePath));
-	console.log(format);
 	return this.bufferToJSON(buf, format);
     }
 
     private bufferToJSON(buf: Uint8Array, format:{}):{} {
+	const br = new BinaryReader(buf);
+	const output: Object = {};
+	for (const [k, v] of Object.entries(format)) {
+	    //console.log(k, v);
+	    this.generateObject(br, v, output);
+	    
+	}
 	return {};
+    }
+
+    private generateObject(br: BinaryReader, v: any , out: {}): {} {
+	if (Array.isArray(v)) {
+	    //gnerateObject(br, k, v, out);
+	}
+	else {
+	    let value = Object.entries(v)[0];
+	    console.log((typeof v));	    
+	    //console.log(">" + value);
+	    this.readBytes(br, value);
+	}
+	return {};	
+    }
+
+    private readBytes(br: BinaryReader, v: any): number {
+	let test = Object.entries(v)[0];
+	console.log("test> " + test);
+	const length  = Number(v);
+	const bytes: Uint8Array = br.readBytes(length);
+	let shift: number = 0x00;
+	console.log(length, bytes);
+	let val: number = bytes.reduce((acc: number, val: number): number => {
+	    let ret = acc | (val << shift);
+	    shift += 0x08;
+	    console.log(ret);
+	    return ret;
+	}, 0);
+
+	return 0;
     }
 }
--- test.ts
+++ test.ts
@@ -18,13 +18,10 @@ Deno.test(
 Deno.test(
     "convert test1",
     function():void {
-	const buffer = new ArrayBuffer(4);
-	const arry = new Uint8Array(buffer);
-	arry[0] = 0x48;
-	arry[1] = 0x65;
-	arry[2] = 0xAC;
-	arry[3] = 0x6C;	
+	const file = Deno.openSync("sample.dat");
+	const buffer = Deno.readAllSync(file);
+	Deno.close(file.rid);
 	const b2j = new BinaryToJSON();
-	assertEquals({}, b2j.convert(arry, "./sample.json"));
+	assertEquals({}, b2j.convert(buffer, "./sample.json"));
     },
 );
