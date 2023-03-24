c82909828e3b0ac9f65142d96fa7696394b14a06
ykisii <ishiiyuuki@gmail.com>
2023-03-24 14:04:07 +0900

    Change indent width

--- binary_to_json.ts
+++ binary_to_json.ts
@@ -2,53 +2,53 @@ import { BinaryReader } from "https://deno.land/x/binary_reader@v0.1.6/mod.ts";
 
 export class BinaryToJSON {
 
-    convert(buf: Uint8Array, format: []) : {} {
-        return this.bufferToJSON(buf, format);
+  convert(buf: Uint8Array, format: []) : {} {
+    return this.bufferToJSON(buf, format);
+  }
+
+  private bufferToJSON(buf: Uint8Array, formats:[]):{} {
+    // needs array. when read binary sequentially.
+    if (Array.isArray(formats) === false) return {};
+
+    const br = new BinaryReader(buf);
+    const output = {};
+
+    for (const format of formats) {
+        this.generateObject(br, format, output);
     }
+    return output;
+  }
 
-    private bufferToJSON(buf: Uint8Array, formats:[]):{} {
-        // needs array. when read binary sequentially.
-        if (Array.isArray(formats) === false) return {};
-
-        const br = new BinaryReader(buf);
-        const output = {};
-
-        for (const format of formats) {
-            this.generateObject(br, format, output);
+  private generateObject(br: BinaryReader, format:{} , out: {}): {} {
+    let obj = {};
+  if (Array.isArray(format)) {
+        console.log("array ....");
+        //gnerateObject(br, k, v, out);
+    }
+    else {
+        const [key, val] = Object.entries(format)[0];
+        console.log(key, val);
+        if (key === "reserve") {
+            // just increase offset.
+            br.readBytes(Number(val));
+            return {};
         }
-        return output;
+        let value: number = this.readBytes(br, Number(val));
+        console.log(value.toString(16));
     }
+    return obj;
+  }
 
-    private generateObject(br: BinaryReader, format:{} , out: {}): {} {
-        let obj = {};
-	    if (Array.isArray(format)) {
-            console.log("array ....");
-    	    //gnerateObject(br, k, v, out);
-        }
-        else {
-            const [key, val] = Object.entries(format)[0];
-            console.log(key, val);
-            if (key === "reserve") {
-                // just increase offset.
-                br.readBytes(Number(val));
-                return {};
-            }
-            let value: number = this.readBytes(br, Number(val));
-            console.log(value.toString(16));
-        }
-        return obj;
-    }
+  private readBytes(br: BinaryReader, length: number): number {
+    const bytes = br.readBytes(length);
+    let shift = 0x00;
+      
+    let val: number = bytes.reduce((acc: number, val: number): number => {
+      let ret = (acc << shift) | val;
+      shift = 0x08;
+      return ret
+    }, 0);
 
-    private readBytes(br: BinaryReader, length: number): number {
-        const bytes = br.readBytes(length);
-        let shift = 0x00;
-        
-        let val: number = bytes.reduce((acc: number, val: number): number => {
-            let ret = (acc << shift) | val;
-            shift = 0x08;
-            return ret
-        }, 0);
-
-        return val;
-    }
+    return val;
+  }
 }
--- test.ts
+++ test.ts
@@ -2,28 +2,27 @@ import { assertEquals, assertNotEquals } from "https://deno.land/std/testing/ass
 import { BinaryToJSON } from "./binary_to_json.ts";
 
 Deno.test(
-    "constructor()",
-    function(): void {
-        const buffer = new ArrayBuffer(4);
-        const arry = new Uint8Array(buffer);
-        arry[0] = 0x48;
-        arry[1] = 0x65;
-        arry[2] = 0xAC;
-        arry[3] = 0x6C;
-        const b2j = new BinaryToJSON();
-        assertNotEquals(null, b2j);
-    },
+  "constructor()",
+  function(): void {
+    const buffer = new ArrayBuffer(4);
+    const arry = new Uint8Array(buffer);
+    arry[0] = 0x48;
+    arry[1] = 0x65;
+    arry[2] = 0xAC;
+    arry[3] = 0x6C;
+    const b2j = new BinaryToJSON();
+    assertNotEquals(null, b2j);
+  },
 );
 
 Deno.test(
-    "convert test1",
-    function():void {
-        const file = Deno.openSync("sample.dat");
-        const buffer = Deno.readAllSync(file);
-        Deno.close(file.rid);
-    
-        const format = JSON.parse(Deno.readTextFileSync("sample.json"));
-        const b2j = new BinaryToJSON();
-        assertEquals({}, b2j.convert(buffer, format));
-    },
+  "convert test1",
+  function():void {
+    const file = Deno.openSync("sample.dat");
+    const buffer = Deno.readAllSync(file);
+    Deno.close(file.rid);
+    const format = JSON.parse(Deno.readTextFileSync("sample.json"));
+    const b2j = new BinaryToJSON();
+    assertEquals({}, b2j.convert(buffer, format));
+  },
 );
